cee32f4472a8a7de49bd4a0e040319aff1686907
devils <zleaves@126.com>
2020-06-01 19:34:17 +0800

    readme.md

--- config/index.js
+++ config/index.js
@@ -1,9 +1,9 @@
 // 数据库配置
 const db={
     host:'x.x.x.x', // 数据库服务器
-    name:'weixin', // 数据库
-    user:'root', // 账号
-    password:'p0o9i8u7' // 密码
+    name:'xx', // 数据库
+    user:'xx', // 账号
+    password:'xx' // 密码
 }
 module.exports={
     db,
